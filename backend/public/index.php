d72eec354447fd68b4a9ce4b2007c86f316e8b1a
wsz92 <winnie.szeto02@gmail.com>
2025-06-30 15:26:20 -0400

    Added a local file 'staffList.json' to store the list of staff members to be displayed

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,1 +1,7 @@
-<?php echo json_encode(["message" => "TESTING"]); ?>
+<?php
+header("Access-Control-Allow-Origin: http://localhost:3000"); # allows frontend to make requests
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); # allows API to use GET, POST, and OPTIONS methods
+header("Access-Control-Allow-Headers: Content-Type"); # allows PHP recieve JSON from the frontend
+header("Content-Type: application/json"); # the code within this PHP file is in JSON format
+
+?>
